df9a943d0ce04344b2e96e030f559bc49f3f58fb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-08-08 00:06:04 +0800

    PidController

---
+++ PidController.m
@@ -0,0 +1,33 @@
+classdef PidController
+	properties
+		KP
+		KI
+		KD
+		Y
+		X
+	end
+	
+	methods
+		function obj = PidController(KP,KI,KD)
+			obj.KP = KP;
+			obj.KI = KI;
+			obj.KD = KD;
+			obj.X = zeros(1,3);
+			obj.Y = 0;
+		end
+		
+		function Input(obj,X)
+			obj.X(3) = obj.X(2);
+			obj.X(2) = obj.X(1);
+			obj.X(1) = X;
+		end
+		
+		function y = Output(obj)
+			p = obj.KP * (obj.X(1) - obj.X(2));
+			i = obj.KI * obj.X(1);
+			d = obj.KD * (obj.X(1) - 2 * obj.X(2) + obj.X(3));
+			obj.Y = p + i + d + obj.Y;
+			y = obj.Y;
+		end
+	end
+end
